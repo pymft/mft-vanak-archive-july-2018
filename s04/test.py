5874fc5edc571038df5d6a06531cbbad2779d428
vee <naeini@pm.me>
2018-05-31 22:55:26 -0700

    Revert "recursive functions"
    This reverts commit b6dbc26

--- s04/test.py
+++ s04/test.py
@@ -1,14 +1,6 @@
-def name_of_function(arg1, arg2, kwarg1=10):
-    return arg1, arg2, kwarg1
+def name_of_function(arg1, arg2):
+    return arg1, arg2
 
-
-def fact(n):
-    if n == 0:
-        return 1
-    return n * fact(n-1)
-
-val = name_of_function(1, 2, kwarg1=100)
+val = name_of_function(1, 2)
 print(val)
 print(type(val))
-
-print("10! = ", fact(10))
