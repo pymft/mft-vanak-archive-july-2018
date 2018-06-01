33f825cc2fc20103a838c8297a8ab5834deb3f89
vee <naeini@pm.me>
2018-05-31 23:37:32 -0700

    map cont.

--- s04/question.py
+++ s04/question.py
@@ -1,4 +1,15 @@
 inp = [1, 2, 3, 4, 100, 2]
-out = [4800 , 2400 , 1600 , 1200 , 48   , 2400 ]
+# out = [4800 , 2400 , 1600 , 1200 , 48   , 2400 ]
 
 # order O
+A = 1
+for val in inp:
+    A *= val
+
+def mapfun(x):
+    return A/x
+
+
+print(A)
+out = map(lambda x: A/x, inp)
+print(list(out))
