b6dbc263cef8847411da8f7bc021299b01e3b67f
vee <naeini@pm.me>
2018-05-31 22:13:02 -0700

    recursive functions

--- s04/test.py
+++ s04/test.py
@@ -1,6 +1,14 @@
-def name_of_function(arg1, arg2):
-    return arg1, arg2
+def name_of_function(arg1, arg2, kwarg1=10):
+    return arg1, arg2, kwarg1
 
-val = name_of_function(1, 2)
+
+def fact(n):
+    if n == 0:
+        return 1
+    return n * fact(n-1)
+
+val = name_of_function(1, 2, kwarg1=100)
 print(val)
 print(type(val))
+
+print("10! = ", fact(10))
