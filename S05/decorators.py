293a5b487c5bfe710c8d7451f8e9a2aa8ae13ab0
Vahid Naeini <naeini.v@gmail.com>
2018-06-14 21:57:58 -0700

    decorators cont. and assignment

--- S05/decorators.py
+++ S05/decorators.py
@@ -1,22 +1,30 @@
 def function_maker(f):
-    def inner_function(arg):
-        res = "<i>" + f(arg) + "</i>"
+    def inner_function(*args):
+        res = "<i>" + str(f(*args)) + "</i>"
         return res
     return inner_function
 
-def another_function_maker(f):
-    def inner_function(arg):
-        res = "<b>" + f(arg) + "</b>"
-        return res
-    return inner_function
-
-# DECORATOR PATTERN
-@function_maker            # function_maker(another_function_maker(echo_text))
-@another_function_maker    # <-----------echo_text---------->
+@function_maker
 def echo_text(s):          # another_function_maker(echo_text)
     return s
+
+@function_maker
+def fact(n):
+    val = 1
+    while n > 0:
+        val *= n
+        n -= 1
+    return val
+
+@function_maker
+def my_pow(a, b):
+    return a ** b
+
+
 
 # echo_text = function_maker(echo_text)
 
 print(echo_text('hello'))
+print(fact(10))
+print(my_pow(2, 10))
 
